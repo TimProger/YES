b4a597dbb1a8ffbea6632f21e233c46322277df0
Taimo <79605237+TimProger@users.noreply.github.com>
2021-09-17 23:05:53 +0300

    Обновил сортированный массив превратив в строку
    Я неправильно понял задание с сортировкой, так-что переделал код и теперь вывожу не массив сортированных объектов, а строки с именем и датой

--- yes/yes.js
+++ yes/yes.js
@@ -1,42 +1,50 @@
 const fs = require("fs");
-let coolArr = []
-let coolArrDateSorted = []
 
+let coolArr = [];
+let coolDateSorted = "";
+class Person {
+  constructor(name, surname, fullname, birthday, gender) {
+    this.name = name;
+    this.surname = surname;
+    this.fullname = fullname;
+    this.birthday = birthday;
+    this.gender = gender;
+  }
+}
 fs.readFile(
-    "ppl.csv",
-    {
-        encoding: "utf-8",
-    },
-    function (err, data) {
-        if (err) {
-            throw new Error(err);
-        } else {
-            class Person {
-                constructor(name, surname, fullname, birthday, gender) {
-                  this.name = name;
-                  this.surname = surname;
-                  this.fullname = fullname;
-                  this.birthday = birthday;
-                  this.gender = gender;
+  "ppl.csv",
+  {
+    encoding: "utf-8",
+  },
+  function (err, data) {
+    if (err) {
+      throw new Error(err);
+    } else {
+      const reg1 = /\n+/g;
+      const reg2 = /\r+/g;
+      let arr = data.replace(reg1, "").replace(reg2, "").split(";;;;");
+      arr.pop();
 
-                }
-              }
-            const reg1 = /\n+/g;
-            const reg2 = /\r+/g;
-            let arr = data.replace(reg1, '').replace(reg2, '').split(";;;;")
-            arr.pop()
+      for (i = 1; i < arr.length; i++) {
+        let map = arr[i].split(";");
+        let person = new Person(
+          map[0],
+          map[1],
+          map[0].split("")[0] + ". " + map[1],
+          map[3],
+          map[2]
+        );
+        coolArr.push(person);
+      }
+      console.log(coolArr);
 
-            for (i = 1; i < arr.length; i++) {
-                let map = arr[i].split(';')
-                let person = new Person(map[0], map[1], map[0].split('')[0] + ". " + map[1], map[3], map[2])
-                coolArr.push(person)
-            }
-            coolArrDateSorted = coolArr.sort(function(a,b){
-                return new Date(b.birthday) - new Date(a.birthday);
-              });
-            console.log(coolArr)
-            console.log(coolArrDateSorted)
-
-        }
+      let sorted = coolArr.sort(function (a, b) {
+        return new Date(b.birthday) - new Date(a.birthday);
+      });
+      sorted.forEach((el) => {
+        coolDateSorted += `${el.fullname}, ${el.birthday}\n`;
+      });
+      console.log(coolDateSorted);
     }
+  }
 );
